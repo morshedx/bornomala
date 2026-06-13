d17abea6cc4051dcfae6cc177a788c5d4c92ef83
morshedx <morshedx@users.noreply.github.com>
2026-06-13 21:49:46 +0600

    Release 0.8.0: cloud Backup & restore to Google Drive

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ plugins {
 }
 
 // App version, reused for the build config and the output APK file name.
-val appVersionName = "0.7.1"
-val appVersionCode = 54
+val appVersionName = "0.8.0"
+val appVersionCode = 55
 
 // Optional release signing config, loaded from a gitignored keystore.properties.
 val keystorePropertiesFile = rootProject.file("keystore.properties")
